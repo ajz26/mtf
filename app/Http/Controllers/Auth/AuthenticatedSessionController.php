d70b50b078354a270abc6d90f3f80ea6a33be7f9
= <zambranoantoo@gmail.com>
2022-11-09 13:47:53 +0100

    implementacion de autenticacion

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param  \App\Http\Requests\Auth\LoginRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(LoginRequest $request)
+    {
+        $request->authenticate();
+
+        $user = $request->user();
+
+        return response()->json([
+            'token' => $user->createToken($request->userAgent() ?? 'auth')->plainTextToken,
+            'message' => __("User authenticated successfully")
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+
+        Auth::guard('web')->logout();
+
+        $success = $request->user()->currentAccessToken()->delete();
+
+
+
+        return response()->noContent();
+    }
+}
